0c494b73553d499c693081a4c4d1871876c2c130
asepopov <asepopov@avito.ru>
2024-11-12 16:59:35 +0300

    12.11.2024

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -5,6 +5,9 @@ import (
 	"file-structure/internal/repository"
 	"file-structure/internal/service"
 	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 // компилируемые (go, c, c++, java)
@@ -17,6 +20,47 @@ func main() {
 
 	handler := api.New(srv)
 
-	res := handler.Handle(10, 2)
-	fmt.Println(res)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("Hello GET!!"))
+		case http.MethodPost:
+			w.WriteHeader(http.StatusCreated)
+			w.Write([]byte("Hello POST!!"))
+		}
+	})
+
+	http.HandleFunc("/user/{id}", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			_ = r.PathValue("id")
+
+			a := r.URL.Query().Get("a")
+			b := r.URL.Query().Get("b")
+			log.Println(a, b)
+			aInt, err := strconv.Atoi(a)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			bInt, err := strconv.Atoi(b)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			res := handler.Handle(aInt, bInt)
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(fmt.Sprintf("%d", res)))
+		}
+	})
+
+	//fmt.Println(res)
+
+	err := http.ListenAndServe(":3232", nil)
+	if err != nil {
+		panic(err)
+	}
 }
